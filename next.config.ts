3e46cc567af4cb8b6eca4b0bf0f79eaed32a5abc
Tan Yong Kuan <yongkuan94@gmail.com>
2024-11-01 17:37:07 +0800

    Fix deprecated

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,13 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
   images: {
-    domains: ['images.unsplash.com']
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        pathname: '**' // This allows all paths from Unsplash
+      }
+    ]
   }
 }
 
